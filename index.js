36cb66f7d4919719d85bd625b36e2e4cf4a52218
Moshe <moisherosenzweig@gmail.com>
2022-02-06 15:21:33 -0500

    commit tree b672998037b11c3631a15eb37341ceb92686880a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+function superBowlWin (array) {
+
+    let found = array.find(array =>(array.result === 'W')); {
+ console.log(found)
+ if (found) {
+     return found.year; 
+ } else {
+     return undefined;
+ }
+    }}
+
+    superBowlWin(array)
